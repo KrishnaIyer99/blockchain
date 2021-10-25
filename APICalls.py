1bd6ac6f9888b97873e811642ca874f205d429bf
KrishnaIyer99 <krishnaiyer28@gmail.com>
2021-10-25 13:48:33 -0400

    refactored backend to compare quotes on multiple exchanges

--- APICalls.py
+++ APICalls.py
@@ -10,56 +10,74 @@ from flask import Flask
 from flask import render_template
 import requests
 import json
+import pandas as pd
 app = Flask(__name__)
 
-def get_btc_prices():
-    resp = {}
-    try:
-        blockchain = requests.get("https://api.blockchain.com/v3/exchange/tickers/BTC-USD").json()
-        coincap = requests.get("http://api.coincap.io/v2/assets/bitcoin").json()
-        coincap = dict(coincap['data'])
+def data_processing(coin_prices):
+    coin_prices = coin_prices["data"]
+    coin_prices_df = pd.DataFrame(coin_prices)
+    
+    coin_prices_df = coin_prices_df.astype({"priceUsd": float})
+    return coin_prices_df
 
-        blockchain_t = (round(float(blockchain['last_trade_price']), 2), "https://www.blockchain.com/")
-        coincap_t = (round(float(coincap['priceUsd']), 2), "https://coincap.io/")
+def get_btc_prices():
+    try:
+        btc_prices = requests.get("http://api.coincap.io/v2/assets/bitcoin/markets").json()
     except:
         return {
-            "BUY":(-1, "https://www.blockchain.com/"),
-            "SELL":(-1, "https://coincap.io/")
+            "BUY": (-1, "Error fetching price, try refreshing"),
+            "SELL": (-1, "Error fetching price, try refreshing")
         }
-        
-    if blockchain_t[0] > coincap_t[0]:
-        resp["BUY"] = coincap_t
-        resp["SELL"] = blockchain_t
-    else:
-        resp["SELL"] = coincap_t
-        resp["BUY"] = blockchain_t
-    
-    return resp
 
+    btc_prices_df = data_processing(btc_prices)
+    btc_prices_df = btc_prices_df.loc[btc_prices_df['baseSymbol'] == "BTC"]
+
+    btc_max_price = btc_prices_df.loc[btc_prices_df['priceUsd'].idxmax()]
+    btc_min_price = btc_prices_df.loc[btc_prices_df['priceUsd'].idxmin()]
+
+    return {
+        "BUY": (round(btc_min_price['priceUsd'], 2), btc_min_price['exchangeId']),
+        "SELL": (round(btc_max_price['priceUsd'], 2), btc_max_price['exchangeId']),
+    }
 
 def get_eth_prices():
-    resp ={}
     try:
-        blockchain = requests.get("https://api.blockchain.com/v3/exchange/tickers/ETH-USD").json()
-        coincap = requests.get("http://api.coincap.io/v2/assets/ethereum").json()
-        coincap = dict(coincap['data'])
-
-        blockchain_t = (round(float(blockchain['last_trade_price']), 2), "https://www.blockchain.com/")
-        coincap_t = (round(float(coincap['priceUsd']), 2), "https://coincap.io/")
+        eth_prices = requests.get("http://api.coincap.io/v2/assets/ethereum/markets").json()
     except:
         return {
-            "BUY":(-1, "https://www.blockchain.com/"),
-            "SELL":(-1, "https://coincap.io/")
+            "BUY": (-1, "Error fetching price, try refreshing"),
+            "SELL": (-1, "Error fetching price, try refreshing")
         }
+    eth_prices_df = data_processing(eth_prices)
+    eth_prices_df = eth_prices_df.loc[eth_prices_df["baseSymbol"] == "ETH"]
 
-    if blockchain_t[0] > coincap_t[0]:
-        resp["BUY"] = coincap_t
-        resp["SELL"] = blockchain_t
-    else:
-        resp["SELL"] = coincap_t
-        resp["BUY"] = blockchain_t
-    
-    return resp
+    eth_max_price = eth_prices_df.loc[eth_prices_df["priceUsd"].idxmax()]
+    eth_min_price = eth_prices_df.loc[eth_prices_df["priceUsd"].idxmin()]
+
+    return {
+        "BUY": (round(eth_min_price['priceUsd'], 2), eth_min_price['exchangeId']),
+        "SELL": (round(eth_max_price['priceUsd'], 2), eth_max_price['exchangeId']),
+    }
+
+def get_doge_prices():
+    try:
+        doge_prices = requests.get("http://api.coincap.io/v2/assets/dogecoin/markets").json()
+    except:
+        return {
+            "BUY": (-1, "Error fetching price, try refreshing"),
+            "SELL": (-1, "Error fetching price, try refreshing")
+        }
+    doge_prices_df = data_processing(doge_prices)
+    doge_prices_df = doge_prices_df.loc[doge_prices_df['baseSymbol'] == "DOGE"]
+
+
+    doge_max_price = doge_prices_df.loc[doge_prices_df['priceUsd'].idxmax()]
+    doge_min_price = doge_prices_df.loc[doge_prices_df['priceUsd'].idxmin()]
+
+    return {
+        "BUY": (round(doge_min_price['priceUsd'], 4), doge_min_price['exchangeId']),
+        "SELL": (round(doge_max_price['priceUsd'], 4), doge_max_price['exchangeId']),
+    }
 
 def display_as_string(price_data):
     if price_data[0] == -1:
@@ -74,13 +92,7 @@ def home():
 
 @app.route("/test")
 def test():
-    btc = get_btc_prices()
-    eth = get_eth_prices()
-
-    return {
-        "BTC":str(btc),
-        "ETH":str(eth)
-    }
+    return get_btc_prices().to_dict()
 
 @app.route("/BTC_PRICE")
 def display_btc_data():
